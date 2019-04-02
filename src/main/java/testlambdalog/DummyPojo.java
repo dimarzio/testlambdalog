e983d8b0b5776330f46f2b0be852ae563192246f
Dmitriy Neretin <dmitriy.neretin@ionity.eu>
2019-04-02 12:39:48 +0200

    Try to log with log4j2

---
+++ src/main/java/testlambdalog/DummyPojo.java
@@ -0,0 +1,14 @@
+package testlambdalog;
+
+public class DummyPojo {
+
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
